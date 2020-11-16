ec5e35dac7d56e48b3c40979e5182e976c7e1589
Danilo da Silva Ataide <62820994+danilosataide@users.noreply.github.com>
2020-11-16 16:58:12 -0300

    metodos http

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,43 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('/Listagem de usuários');
+app.use(express.json());
 
-    response.send('teste');
+// POST http://localhost:3333/users = Criar um usuário
+// GET http://localhost:3333/users = Listar usuários
+// GET http://localhost:3333/users/5 = Buscar dados do usuário com ID 5
+
+// Request Param: Parâmetros que vem na própria rota que identificam um recurso
+// Query Param: Parâmetros Opcionais na rota
+// Request Body: Parâmetros para criação/atualização de informações de entidades
+
+const users = ['Daniel', 'José', 'Marcos'];
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+    return response.json(user)
+});
+
+app.get('/users', (request, response) => {
+    const search = String(request.query.search);
+    
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+    
+    response.send(filteredUsers);
+});
+
+app.post('/users',  (request, response) => {
+    const data = request.body;
+    
+    const user = {
+        name: 'Danilo',
+        email: 'danilo@gmail.com'
+        
+    };
+
+    return response.json(user);
 });
 
 app.listen(3333);
