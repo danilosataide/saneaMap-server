54215b37daef42f7bedeee71262241665ef2c230
Danilo da Silva Ataide <62820994+danilosataide@users.noreply.github.com>
2020-11-16 17:12:15 -0300

    separação de server e routes

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
 
 // POST http://localhost:3333/users = Criar um usuário
 // GET http://localhost:3333/users = Listar usuários
@@ -11,34 +15,3 @@ app.use(express.json());
 // Request Param: Parâmetros que vem na própria rota que identificam um recurso
 // Query Param: Parâmetros Opcionais na rota
 // Request Body: Parâmetros para criação/atualização de informações de entidades
-
-const users = ['Daniel', 'José', 'Marcos'];
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id);
-
-    const user = users[id];
-    return response.json(user)
-});
-
-app.get('/users', (request, response) => {
-    const search = String(request.query.search);
-    
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-    
-    response.send(filteredUsers);
-});
-
-app.post('/users',  (request, response) => {
-    const data = request.body;
-    
-    const user = {
-        name: 'Danilo',
-        email: 'danilo@gmail.com'
-        
-    };
-
-    return response.json(user);
-});
-
-app.listen(3333);
